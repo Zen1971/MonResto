3f9fa2e4f17fbe09e699f695127ee972ab366de1
Zen1971 <olstephnat@gmail.com>
2024-12-05 12:02:16 +0100

    Initial commit

---
+++ JS/stickyscroll.js
@@ -0,0 +1,14 @@
+//détecter lorsque l'on scroll
+window.onscroll = function() {sticky()};
+//récupérer le header par l'ID
+let header = document.getElementById('sticky-header');
+//récupérer la position du menu sur la page
+let stickyComponent = header.offsetTop;
+//faire la fonction
+function sticky(){
+    if(window.scrollY > stickyComponent){
+        header.classList.add('sticky');
+    }else{
+        header.classList.remove('sticky');
+    }
+}
