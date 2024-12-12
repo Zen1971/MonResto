27691ecaa6c5a477f035e5e0e6ec63575a3bf061
Zen1971 <olstephnat@gmail.com>
2024-12-12 11:55:37 +0100

    Modification js

--- JS/stickyscroll.js
+++ JS/stickyscroll.js
@@ -1,6 +1,6 @@
 //détecter lorsque l'on scroll
 window.onscroll = function() {sticky()};
-//récupérer le header par l'ID
+//récupérer le header par l'ID de sticky-header
 let header = document.getElementById('sticky-header');
 //récupérer la position du menu sur la page
 let stickyComponent = header.offsetTop;
